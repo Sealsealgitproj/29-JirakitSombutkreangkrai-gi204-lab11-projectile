7fd4fde5797e3f05de307ff57f25b2bea3ca84b3
Sealsealgitproj <Jirakit.somb@bumail.net>
2025-04-28 21:00:35 +0700

    Version 2
    Tinkering with scripts

--- Assets/Scenes/Scripts/Script1.cs
+++ Assets/Scenes/Scripts/Script1.cs
@@ -1,20 +1,17 @@
 using UnityEngine;
-
 public class Projectile2D : MonoBehaviour
 {
     [SerializeField] Transform shootPoint;
-    [SerializeField] GameObject target; //target sprite [SerializeField] Rigidbody2D bulletPrefab;
+    [SerializeField] GameObject target;
+    [SerializeField] Rigidbody2D bulletPrefab;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.R))
         {
-//shoot raycast to detect mouse clicked position
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.red, 5f);
-//get click point
+            Debug.DrawRay(ray.origin, ray.direction * 5f, Color.blue, 5f);
             RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
-//if hit object with collider
             if (hit.collider != null)
             {
                 target.transform.position = new Vector2(hit.point.x, hit.point.y);
